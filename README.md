36b907d1b39b2f68727e9471214d40a27f754a43
gergo <kiss.gergely@gmail.com>
2024-03-23 17:36:58 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Development
+
+`npm i`
+
+`npm run dev`
+
+# Deployment
+
+```
+./node_modules/.bin/wrangler login
+./deploy.sh
+```
+
+To prod:
+
+```
+./deploy.sh production
+```
---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { link, Route, Router } from "svelte-routing";
+  import PracticeJLy from "./PracticeJLy.svelte";
+
+  export let url = "";
+</script>
+
+<main>
+  <Router {url}>
+    <nav>
+      <a href="/" class="link" use:link>J vagy Ly</a>
+    </nav>
+
+    <Route path="/"><PracticeJLy /></Route>
+  </Router>
+</main>
+
+<style>
+  nav {
+    font-size: small;
+  }
+
+  nav a + a:before {
+    content: "|";
+    padding: 0 0.2em;
+  }
+</style>
